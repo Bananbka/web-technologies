94b410878bddebcd436c46bd38edf1917b3b77fa
Bananbka <bananbka0.2@gmail.com>
2025-09-14 11:35:40 +0300

    husky fail test

---
+++ lab3/test/test.js
@@ -0,0 +1,7 @@
+﻿const { expect } = require('chai');
+
+describe('Failing test example', () => {
+    it('this test should fail', () => {
+        expect(2 + 2).to.equal(5); // Це провалиться
+    });
+});
